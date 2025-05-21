32063cdef2b24e8381ff921b748b97defc338a49
SQsuqi <643352633@qq.com>
2025-05-21 13:46:48 +0800

    12321

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 
 const app = express()
+const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello World12211!')
+})
+
+app.listen(port, () => {
+  console.error(`Example app listening on port ${port}`)
 })
 
 export default app
