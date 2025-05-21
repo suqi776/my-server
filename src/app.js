eca64f5d0ea4f3eb2568ab9f2f06ca2a5e0f9bd7
SQsuqi <643352633@qq.com>
2025-05-21 18:07:49 +0800

    111

--- src/app.js
+++ src/app.js
@@ -4,6 +4,13 @@ import tangRouter from './router/tang.js'
 const app = express()
 const port = 3000
 
+// 允许所有来源跨域（开发时用，生产请限制）
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*') // 允许所有来源跨域
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 app.get('/', (req, res) => {
   res.send('Hello World12211!')
 })
